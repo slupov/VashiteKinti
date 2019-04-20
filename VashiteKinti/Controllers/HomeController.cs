84780d10c6ec413e7f022aefeb3d10c7002e3e91
Slupov <stoyan.lupov@gmail.com>
2019-04-20 22:08:42 +0300

    [Fixed bug with wrong identity user for UserManager]

--- VashiteKinti/Controllers/HomeController.cs
+++ VashiteKinti/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using VashiteKinti.Models;
 
-namespace VashiteKinti.Controllers
+namespace VashiteKinti.Web.Controllers
 {
     public class HomeController : Controller
     {
